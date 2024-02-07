10e8abb3940b83aac74ebdf045f10feaae9fccd1
RDX RAJ <141653116+NOBITA-RDX@users.noreply.github.com>
2024-02-07 12:10:14 +0530

    Add files via upload

---
+++ VDjRDXMUSIC/__init__.py
@@ -0,0 +1,28 @@
+from VDjRDXMUSIC.core.bot import DAXX
+from VDjRDXMUSIC.core.dir import dirr
+from VDjRDXMUSIC.core.git import git
+from VDjRDXMUSIC.core.userbot import Userbot
+from VDjRDXMUSIC.misc import dbb, heroku
+
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = RDX()
+api = SafoneAPI()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
